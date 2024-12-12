59e7c8be6ac0542abaabd278fd25d548a6f5665b
z58occc <a0981583030@gmail.com>
2024-12-13 02:57:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { HashRouter } from 'react-router';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
